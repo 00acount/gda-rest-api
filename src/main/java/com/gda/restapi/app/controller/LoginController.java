82b332134a6ebd7c187f5a7669c1e33e9dda9234
00acount <146901089+00acount@users.noreply.github.com>
2023-10-11 17:50:54 +0100

    Update LoginController.java

--- src/main/java/com/gda/restapi/app/controller/LoginController.java
+++ src/main/java/com/gda/restapi/app/controller/LoginController.java
@@ -23,7 +23,10 @@ public class LoginController {
 
 	@GetMapping("/refresh-xsrf")
 	public void refreshXsrf(HttpServletResponse response) {
-		response.addHeader("Set-Cookie", "SIDCC=ACA-OxPTyKIPs6b-lm6iIwU-XTngrACH-axtvLYgtDDjxadatmsLJijQJfVQozC8qcqerCjFhbFq; expires=Thu, 10-Oct-2024 15:55:59 GMT; path=/; domain=.onrender.com");
+		response.addHeader("Set-Cookie", "SIDCC=1; expires=Thu, 10-Oct-2024 15:55:59 GMT; path=/; domain=gdaapi.onrender.com");
+		response.addHeader("Set-Cookie", "SIDCC=2; expires=Thu, 10-Oct-2024 15:55:59 GMT; path=/; domain=.gdaapi.onrender.com");
+		response.addHeader("Set-Cookie", "SIDCC=3; expires=Thu, 10-Oct-2024 15:55:59 GMT; path=/; domain=onrender.com");
+		response.addHeader("Set-Cookie", "SIDCC=4; expires=Thu, 10-Oct-2024 15:55:59 GMT; path=/; domain=.onrender.com");
 	}
 	
 	@PostMapping("/login")
