a46bedf92d1d73b8931d5aa5c04f0c64d663a96e
00account <00accont@gmail.com>
2023-10-12 04:05:08 +0100

    udapte

--- src/main/java/com/gda/restapi/app/configuration/CorsConfig.java
+++ src/main/java/com/gda/restapi/app/configuration/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://" + frontendDomain + ":5173"));
+		configuration.setAllowedOrigins(Arrays.asList(frontendDomain));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.addExposedHeader("TOKEN");
